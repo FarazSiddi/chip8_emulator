bdc2c81940061bfa6398e17384c022c864d57c19
null-2020 <f1.siddiqui@yandex.com>
2024-01-07 21:28:59 -0500

    Updated Files

--- Constants.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-const unsigned int KEY_COUNT = 16;
-const unsigned int VIDEO_WIDTH = 64;
-const unsigned int VIDEO_HEIGHT = 32;
--- Mode.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-enum class Mode {
-    CHIP8,
-    CHIP48,
-    SCHIP 
-};
